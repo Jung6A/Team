e8e9edae4ff5a0d7d6832dfff8be3ae37efa5465
davidujh <wogkr8301@gmail.com>
2024-09-04 14:30:12 +0900

    로그인 완료

--- src/main/java/com/guestbook/Entity/Member.java
+++ src/main/java/com/guestbook/Entity/Member.java
@@ -2,7 +2,6 @@ package com.guestbook.Entity;
 
 
 import com.guestbook.constant.Role;
-import com.guestbook.constant.Role;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,22 +10,25 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
-@Table(name="project_member")
+@Table(name="project_member ")
 public class Member {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id")
     private Long id;
 
-    @Column(unique=true)
+    @Column(unique = true)
     private String userId;
     private String password;
+    private String name;
 
-    @Column(unique=true)
+    @Column(unique = true)
     private String email;
 
 
     @Enumerated(EnumType.STRING)
     private Role role;
+    private LocalDateTime regTime; //회원가입 날짜와 시간 저장
+    private LocalDateTime updateTime; // 회원 정보 수정 날짜오 시간 저장
 
 }
