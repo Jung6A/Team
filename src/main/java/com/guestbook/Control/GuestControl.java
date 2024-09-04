c5444d804d1e30b2914f127803183ec747f30793
jung6A <heide6@naver.com>
2024-09-04 12:48:19 +0900

    간단한 추가

--- src/main/java/com/guestbook/Control/GuestControl.java
+++ src/main/java/com/guestbook/Control/GuestControl.java
@@ -2,8 +2,15 @@ package com.guestbook.Control;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class GuestControl {
+
+    @GetMapping("/guest")
+    public String guestHome(Model model) {
+        return "guestbook/detail";
+    }
 }
