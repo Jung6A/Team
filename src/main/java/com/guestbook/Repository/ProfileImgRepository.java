be80d11171c189b2da391afa7d8319f18455d45a
Appinnn <dltkdals759@gmail.com>
2024-09-09 11:30:46 +0900

    데이터베이스 파일 경로 저장 수정

--- src/main/java/com/guestbook/Repository/ProfileImgRepository.java
+++ src/main/java/com/guestbook/Repository/ProfileImgRepository.java
@@ -1,19 +1,10 @@
 package com.guestbook.Repository;
 
-import com.guestbook.Dto.ProfileImgDto;
 import com.guestbook.Entity.ProfileImg;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface ProfileImgRepository extends JpaRepository<ProfileImg, Long> {
-
-    static List<ProfileImg> findItem(){
-
-        return List.of();
-    }
-
-    ProfileImg findByItemIdAndRepImgYn(long id, String y);
+    ProfileImg findByIdAndRepImgYn(Long id, String repImgYn);
 }
