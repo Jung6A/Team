0788659456a06c1f86dbc523458143813da3403e
Appinnn <dltkdals759@gmail.com>
2024-09-13 09:43:33 +0900

    Modified files after pull

---
+++ src/main/java/com/guestbook/Service/ReportService.java
@@ -0,0 +1,39 @@
+package com.guestbook.Service;
+
+import com.guestbook.Entity.Guestbook;
+import com.guestbook.Entity.Member;
+import com.guestbook.Entity.Report;
+import com.guestbook.Repository.GuestRepository;
+import com.guestbook.Repository.MemberRepository;
+import com.guestbook.Repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private GuestRepository guestRepository;
+
+    public void submitReport(Long reporterId, Long guestId, String reason, String reasonDetail) {
+        Member reporter = memberRepository.findById(reporterId)
+                .orElseThrow(() -> new IllegalArgumentException("신고자가 존재하지 않습니다."));
+        Guestbook reportedGuest = guestRepository.findById(guestId)
+                .orElseThrow(() -> new IllegalArgumentException("신고당한 사용자가 존재하지 않습니다."));
+
+        Report report = new Report();
+        report.setReporter(reporter);
+        report.setReportedGuest(reportedGuest);
+        report.setReason(reason);
+        report.setReasonDetail(reasonDetail);
+
+        reportRepository.save(report);
+    }
+}
+
